49a47d75e1969cce785eb98ca0b3845f1b110987
Leo Li <leo.li@titansoft.com.sg>
2021-08-23 23:10:12 +0800

    add MoneyRecord

---
+++ Models/MoneyRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_SkillTree.Models
+{
+    public class MoneyRecord
+    {
+        public string Category { get; set; }
+        public string Money { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+    }
+}
