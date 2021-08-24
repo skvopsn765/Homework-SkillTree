61eb07172bd5e603901aedf36117dd6fc35d1c91
Leo Li <leo.li@titansoft.com.sg>
2021-08-24 22:59:39 +0800

    modify use LabelFor and add DisplayName attribute

--- Models/MoneyRecordViewModel.cs
+++ Models/MoneyRecordViewModel.cs
@@ -1,10 +1,19 @@
-﻿namespace Homework_SkillTree.Models
+﻿using System.ComponentModel;
+
+namespace Homework_SkillTree.Models
 {
     public class MoneyRecordViewModel
     {
+        [DisplayName("類別")]
         public string Category { get; set; }
+        
+        [DisplayName("金額")]
         public string Money { get; set; }
+        
+        [DisplayName("日期")]
         public string Date { get; set; }
+        
+        [DisplayName("備註")]
         public string Description { get; set; }
     }
 }
