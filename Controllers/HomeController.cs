8345b7231c92f5ada764d26ab00a5c881a82235b
Leo Li <leo.li@titansoft.com.sg>
2021-08-24 23:28:35 +0800

    modify to use EnumDropDownListFor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,32 +9,16 @@ namespace Homework_SkillTree.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<MoneyRecordViewModel> _moneyRecords = new List<MoneyRecordViewModel>();
 
         public ActionResult Index()
         {
-            SetViewDataSelectList();
             return View();
         }
         
         [HttpPost]
-        public ActionResult Index(string category, string money, string date, string description)
+        public ActionResult Index(MoneyRecordViewModel model, string category, string money, string date, string description)
         {
-            SetViewDataSelectList();
-            _moneyRecords.Add(new MoneyRecordViewModel() { Category = category, Money = money, Date = date, Description = description });
             return View();
-        }
-        
-        private void SetViewDataSelectList()
-        {
-            var items = new List<SelectListItem>()
-            {
-                new SelectListItem() { Text = "請選擇", Value = "請選擇", Selected = true },
-                new SelectListItem() { Text = "支出", Value = "支出" },
-                new SelectListItem() { Text = "收入", Value = "收入" },
-            };
-
-            ViewData["dr"] = items;
         }
 
         public ActionResult About()
--- Models/MoneyRecordViewModel.cs
+++ Models/MoneyRecordViewModel.cs
@@ -1,11 +1,24 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Homework_SkillTree.Models
 {
+    public enum EnumCategory
+    {
+        [Display(Name = "請選擇")]
+        Default,
+        
+        [Display(Name = "收入")]
+        Income,
+        
+        [Display(Name = "支出")]
+        Outcome,
+    }
     public class MoneyRecordViewModel
     {
         [DisplayName("類別")]
-        public string Category { get; set; }
+        public EnumCategory Category { get; set; }
         
         [DisplayName("金額")]
         public string Money { get; set; }
@@ -16,4 +29,6 @@ namespace Homework_SkillTree.Models
         [DisplayName("備註")]
         public string Description { get; set; }
     }
+    
+    
 }
